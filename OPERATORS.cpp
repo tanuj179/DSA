65bedf8a9e5374c580c73943d33d8ee7d92db777
TANUJ GATTANI <72118636+tanuj179@users.noreply.github.com>
2022-08-15 01:30:50 +0530

    DAY1_OPERATORS
    OPERATORS 
    ARITHEMETIC
    RELATIONAL
    LOGICAL
    BITWISE

---
+++ OPERATORS.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*//arithemetic +,-,/,%,*
+int a=2/5;//it will give 0
+float b=2.0/5.0;//it will give00.4
+cout<<a<<endl;
+cout<<b<<endl;*/
+//RELATIONAL OPERATOR
+int a=2;
+int b=3;
+bool first=(a==b);
+cout<<first<<endl;//FALSE REPRESENT 0
+bool s=(a>b);
+cout<<s<<endl;//FALSE 0
+bool t=(a<b);
+cout<<t<<endl;//True represent 1
+bool f=(a>=b);
+cout<<f<<endl;//FALSE 0
+bool fi=(a<=b);
+cout<<fi<<endl;//TRUE 1
+//LOGICAL OPERATOR
+//BITWISE OPERATOR
+}
