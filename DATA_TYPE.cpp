f2e2ff02361da9bc723f9e1cb7928490b4990cc2
TANUJ GATTANI <72118636+tanuj179@users.noreply.github.com>
2022-08-15 01:29:26 +0530

    DAY1_DATATYPE
    DATA TYPE IN C++

---
+++ DATA_TYPE.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //DATA TYPE
+   /* int a=123;
+    cout<<a<<endl;
+    char b='a';
+    cout<<b<<endl;
+    bool b1=true;
+    cout <<b1<<endl;
+    float f=1.2;
+    cout<<f<<endl;
+    double d=1.23;
+    cout<<d<<endl;
+    int size=sizeof(d);
+    cout<<"size of double is: "<<size<<endl;
+    //char a='v'
+    //cout<<a<<endl;// note: previous declaration as 'int a'
+  */
+  //ASCII VALUE
+  //TYPE CASTING
+ /* int a='a';
+  cout<<a<<endl;
+  char ch=99;
+  cout<<ch<<endl;
+  char ch1=123456;
+  cout<<ch1<<endl;//overflow in implicit constant conversion
+*/
+    unsigned int a=52;//positve number only
+    cout<<a<<endl;
+}
